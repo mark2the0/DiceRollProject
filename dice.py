73c305f24de77d7307ef60b08356bc39f1aa9ff8
mark2the0 <mark2the0@icloud.com>
2025-01-24 14:05:17 +1100

    commit dice_roll_function

--- dice.py
+++ dice.py
@@ -1,1 +1,5 @@
 # put your dice_roll() function in this file
+import random
+
+def dice_roll(faces):
+    return random.randrange(1, faces)
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from pyscript import document  # webpage module links
 # your roll_dice function should be saved in a file named 'dice.py'
 # uncomment the next line when you have this prepared
-# import dice
+import dice
 
 
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
-dice_type = "Coin"
+dice_type = "2"
 
 
 def select_face_option(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
+    dice_type = int(event.target.value)
 
 
 def roll_all_dice(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
+    result = dice.dice_roll(dice_type)
+    document.querySelector("div#roll-history").innerHTML = "<p id='something'>" + str(result) + ""
 
 
 def clear_history(event):
